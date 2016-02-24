b9371e7aa2f6825c6c1acbdee9ae09e52a185750
Timothy Madden <tmadden@anl.gov>
2016-02-24 11:30:57 -0600

    add iocPCO start up files and dir
    remove the dexela dir. add iocPCO, and useful start up fiiles for sec2
    windows boxes. also add linux st files.

---
+++ iocs/pcoIOC/iocBoot/iocPCO/STARTDIMAX64.cmd
@@ -0,0 +1,12 @@
+D:
+set PATH=D:\EPICS\ADepics\SynApps_5_5\support\ADIOCs\bin\windows-x64-debug;%PATH%
+
+set EPICS_CA_MAX_ARRAY_BYTES=40000000
+set EPICS_DISPLAY_PATH=D:\EPICS\ADEpics\synApps_5_5\support\all_adl
+ 
+  cd D:\EPICS\ADepics\SynApps_5_5\support\ADIOCs\iocBoot\iocPCO2
+  
+  ..\..\bin\windows-x64-debug\PCOApp.exe PCO_st64.cmd
+  
+  
+  
